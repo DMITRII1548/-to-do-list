5c493ab2fc11511898cd3bda5937a5f679d59d0e
Dmitrii <korsunov2006@list.ru>
2022-08-03 13:07:08 +0600

    search folder

--- app/Http/Controllers/Search/BaseSearchController.php
+++ app/Http/Controllers/Search/BaseSearchController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Search;
 
+use App\Http\Controllers\Controller;
 use App\Services\Search\SearchService;
 use Illuminate\Http\Request;
 
